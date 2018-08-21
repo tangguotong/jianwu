48d7e0fa6955f018fd11da3bded309fcfbaef74b
tangguotong <tangguotong@163.com>
2018-08-21 11:42:36 +0800

    修改头部样式,滚动条样式,左侧结构

--- src/js/openCases/caseReport.js
+++ src/js/openCases/caseReport.js
@@ -5,6 +5,8 @@ $(function () {
         time: 500,
         title: ["案件受理", "证据审查", "事实认定", "证据充分性判断", "量刑计算","逮捕必要性评估","案案件监督"]
     });
+
+    showNext();//监听屏幕滚动.显示固定头部的下一步
     //table受理必要材料
     $("#need").bootstrapTable({
         columns:[
--- src/js/openCases/caseSubmit.js
+++ src/js/openCases/caseSubmit.js
@@ -16,9 +16,7 @@ $(function () {
         },
         drop:function(e){
             e.preventDefault();
-            //拖拽结束后,获取被拖拽的文件
-            var files = event.dataTransfer.files;
-            console.log(files);
+            $("#files").click();//调用input的函数
         },
         dragenter:function(e){
             e.preventDefault();
@@ -31,14 +29,22 @@ $(function () {
 
     //点击上传文件,触发input[type = file]的点击事件
     $('#load').click(function(){
-        $('#load_xls').trigger('click');
+        $("#files").click();//调用input的函数
     });
 
+
 });
-
-
-function uploadFile(file) {
-    var myform = new FormData();
-    myform.append('file',$('#load_xls')[0].files[0]);
-
+function fileimport(){
+    var selectedFile = document.getElementById("files").files[0];//获取读取的File对象
+    console.log(selectedFile.length);
+    var name = selectedFile.name;//读取选中文件的文件名
+    var size = selectedFile.size;//读取选中文件的大小
+    console.log("文件名:"+name+"大小："+size);
+    var reader = new FileReader();//这里是核心！！！读取操作就是由它完成的。
+    reader.readAsText(selectedFile,'gb2312');//读取文件的内容，注意编码方式
+    reader.onload = function(){
+        console.log(this.result);//当读取完成之后会回调这个函数，然后此时文件的内容存储到了result中。直接操作即可。
+        $('<pre>' + this.result + '</pre>').appendTo('body');
+        txtdata = this.result;
+    };
 }
