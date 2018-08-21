03661cc7d465357d795c1172d2b2ae22896811b6
maguofeng <18435145162@163.com>
2018-08-21 11:01:03 +0800

    '收起面板'

--- src/js/examineArrest/factualApproval.js
+++ src/js/examineArrest/factualApproval.js
@@ -1,7 +1,3 @@
 $(function () {
-    $(".tab0li").click(function(){
-    	$(this).addClass("activetwo").siblings().removeClass("activetwo");
-    	$(this).children(".circle").addClass("activethree");
-    	$(this).siblings().children(".circle").removeClass("activethree")
-    })
+    
 });
