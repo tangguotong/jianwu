efd7ccf1c04390c82d5d537c4088342c24d8b3c1
tangguotong <tangguotong@163.com>
2018-08-21 19:42:25 +0800

    调整页面

---
+++ src/js/examineArrest/SentencingCalculation.js
@@ -0,0 +1,16 @@
+$(function () {
+    showNext();
+
+    var $step = $("#step");
+    $step.step({
+        index: 0,
+        time: 500,
+        title: ["案件受理", "证据审查", "事实认定", "证据充分性判断", "量刑计算","逮捕必要性评估","案案件监督"]
+    });
+
+    $("input").iCheck({
+        checkboxClass: 'icheckbox_square-blue',
+        radioClass: 'iradio_square-blue',
+        increaseArea: '20%' // optional
+    });
+})
