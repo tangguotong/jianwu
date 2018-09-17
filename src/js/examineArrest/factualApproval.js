aa29b532f54d6533319c64f94c0df6ec83e22d3b
tangguotong <tangguotong@163.com>
2018-09-17 18:03:12 +0800

    新添加页面

--- src/js/examineArrest/factualApproval.js
+++ src/js/examineArrest/factualApproval.js
@@ -13,26 +13,49 @@ $(function () {
         radioClass: 'iradio_square-blue',
         increaseArea: '20%' // optional
     });
+    var examining = "<span class='icon-box'><i class='icon iconfont icon-yinzhang1 fc-green'></i>正在审查</span>";//正在审查
+    var examined = "<span class='icon-box'><i class='icon iconfont icon-time-fill fc-orange'></i>已审查</span>";//已审查
+    var noexamine = "<span class='icon-box'><i class='icon iconfont icon-hulve fc-gray-deep'></i>未审查</span>";//未审查
+    var data = [
+        {
+            field0:"王晓斌",
+            field1:examining,
+            field2:'--',
+            field3:noexamine,
+        },
+        {
+            field0:"王晓斌1",
+            field1:examined,
+            field2:noexamine,
+            field3:noexamine,
+        }
+    ];
+    setTrueTable(data);//事实审定总览函数
 
 
-
-       $(".tab0li").click(function(){
-       	$(this).addClass("activetwo").siblings().removeClass("activetwo");
-       	$(this).children(".circle").addClass("activethree");
-       	$(this).siblings().children(".circle").removeClass("activethree")
-       })
     
-    $(".jzevidence .textareaw").focus(function(){
-           $(this).parents().find(".contrast").fadeIn();
-      });
-    $(".jzevidence .textareaw").blur(function(){
-           $(this).parents().find(".contrast").fadeOut();
-    });
-    
-     $(".ltevidence .textareaws").focus(function(){
-           $(this).siblings().find(".contrasts").fadeIn();
-      });
-    $(".ltevidence .textareaws").blur(function(){
-           $(this).parents().find(".contrasts").fadeOut();
-     });
+    //事实审定总览函数
+    function setTrueTable(data) {
+        $("#truthTable").bootstrapTable({
+            columns:[
+                {
+                    field:"field0",
+                    title:"罪名/犯罪嫌疑人"
+                },
+                {
+                    field:"field1",
+                    title:"盗窃罪"
+                },
+                {
+                    field:"field2",
+                    title:"故意伤害罪"
+                },
+                {
+                    field:"field3",
+                    title:"掩饰隐藏犯罪所得"
+                },
+            ],
+            data:data
+        })
+    }
 });
